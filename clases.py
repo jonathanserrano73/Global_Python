34a3af2d667d3b67d19eb7fc4b87933b3ce82227
jonathanserrano73 <serranojonathangabriel72@outlook.es>
2024-11-14 22:22:54 -0300

    Correcciones de codigo y agragar Sanador

--- clases.py
+++ clases.py
@@ -1,27 +1,30 @@
+import random
+
 class Detector:
-    bases_nitrogenadas=["A","T","C","G"]
     
     def __init__(self,matriz_adn):
         self.matriz_adn=matriz_adn
         self.tamaño=len(matriz_adn)
+        self.posicion_mutante=[]
     
     def detectar_mutantes (self):
-        for bases in self.matriz_adn:
-            for base in bases:
-                pass
-                
+        if self.detectar_horizontal() or self.detectar_vertical() or self.detectar_diagonal():
+            return True
+        else:
+            return False
             
     def detectar_horizontal(self):
-        for bases in self.matriz_adn:
+        for fila_inicio,bases in enumerate(self.matriz_adn):
             contador=0
             base_anterior=None
-            for base in bases:
+            for columna_inicio, base in enumerate(bases):
                 if base==base_anterior:
                     contador+=1  
                 else:
                     contador=1
                 base_anterior=base
                 if contador >=4:
+                    self.posicion_mutante.append((fila_inicio,columna_inicio-3))
                     return True
         return False
                     
@@ -36,12 +39,13 @@ class Detector:
             for fila in range(filas):
                 base=self.matriz_adn[fila][columna]
                 
-                if base in base_anterior:
+                if base == base_anterior:
                     contador_bases+=1
                 else:
                     contador_bases=1
             
                 if contador_bases>=4:
+                    self.posicion_mutante.append((fila-3,columna))
                     return True
         return False
                 
@@ -65,11 +69,10 @@ class Detector:
                         contador_bases=1
                     base_anterior=base
                     if contador_bases>=4:
+                        self.posicion_mutante.append((i-3,columna_inicial))
                         return True
-                    else:
-                        return False
                     
-        for columna_inicial in reversed(len(columnas)):
+        for columna_inicial in reversed(range(columnas)):
             contador_bases=0
             base_anterior=None
             for i in range(filas):
@@ -81,38 +84,126 @@ class Detector:
                         contador_bases=1
                     base_anterior=base
                     if contador_bases>=4:
+                        self.posicion_mutante.append((i-3,columna_inicial))
                         return True
-                    else:
-                        return False                    
+        return False                    
     
 class Mutador:
-    base_nitrogenada=""
-    def __init__(self):
-        pass
+    
+    def __init__(self,matriz_adn,base_nitrogenada,posicion_inicial):
+        self.matriz_adn=matriz_adn
+        self.base_nitrogenada=base_nitrogenada
+        self.posicion_inicial=posicion_inicial
+        self.tamaño=len(self.matriz_adn)
     
     def crear_mutante(self):
         pass
     
 class Radiacion(Mutador):
-    base_nitrogenada=""
     
-    def __init__(self):
-        pass
-    def crear_mutante(self, base_nitrogenada, posicion_inicial, orientacion_de_la_mutacion):
-        pass
+    def __init__(self,matriz_adn,base_nitrogenada,posicion_inicial,tamaño, orientacion_de_la_mutacion):
+        super().__init__(matriz_adn,base_nitrogenada,posicion_inicial, tamaño)
+        self.orientacion_de_la_mutacion=orientacion_de_la_mutacion
+        
+    def crear_mutante(self):
+        
+        try:
+            fila_inicial,columna_inicial=self.posicion_inicial
+            if not (0<=fila_inicial<self.tamaño and 0<=columna_inicial<len(self.matriz_adn[0])):
+                raise ValueError("La posicion inicial esta fuera de los limites de la matriz")
+            if self.orientacion_de_la_mutacion=="H":
+                if fila_inicial<self.tamaño:
+                    '''Revisamos si hay espacio para la mutacion'''
+                    if columna_inicial+3 <len(self.matriz_adn[fila_inicial]):
+                        fila_mutada=list(self.matriz_adn[fila_inicial])  #convierte la fila en una lista
+                        '''Remplaza las bases con la base indicada'''
+                        fila_mutada[columna_inicial:columna_inicial+4]=[self.base_nitrogenada]*4
+                        self.matriz_adn[fila_inicial]=''.join(fila_mutada)
+                    else:
+                        raise ValueError ("No hay suficiente espacio en la fila para añadir mutación")
+            elif self.orientacion_de_la_mutacion=="V":
+                if fila_inicial<self.tamaño and columna_inicial<len(self.matriz_adn[fila_inicial]):
+                    
+                    if fila_inicial+3 <self.tamaño:
+                        for i in range(4):
+                            fila_mutada=list(self.matriz_adn[fila_inicial+i])
+                            fila_mutada[columna_inicial]=self.base_nitrogenada
+                            self.matriz_adn[fila_inicial+i]=''.join(fila_mutada)
+                    else:
+                        raise ValueError("No hay suficiente espacio en la columna para añadir mutación")
+            else:
+                raise ValueError ("Error, orientacion no valida, debe ingresar 'H' o 'V'")
+            
+            return self.matriz_adn
+        except ValueError as e:
+            print(f"Error {e}")
+            return None
+        
 
 class Virus(Mutador):
     base_nitrogenada=""
-    def __init__(self):
-        pass
+    def __init__(self,matriz_adn, posicion_inicial, base_nitrogenada,tamaño):
+        super().__init__(matriz_adn, posicion_inicial, base_nitrogenada, tamaño)
     
     def crear_mutante(self, base_nitrogenada, posicion_inicial):
-        pass
 
-class Sanador:
-    
-    def __init__(self):
-        pass
+        try:
+            fila_inicial,columna_inicial=posicion_inicial
+            
+            if not (0<=fila_inicial<self.tamaño and 0<=columna_inicial<len(self.matriz_adn[0])):                        
+                raise ValueError("La posicion inicial esta fuera de los limites de la matriz")
+
+            for i in range(self.tamaño):
+                fila=fila_inicial+i
+                columna=columna_inicial+i
+                if fila<self.tamaño or columna<len(self.matriz_adn[0]):
+                    raise ValueError("No hay suficiente espacio en la fila o columna para añadir la mutación")
+                
+                self.matriz_adn[fila][columna]=base_nitrogenada
+            return self.matriz_adn        
+        except ValueError as e:
+            print(f"Error {e}")
+            return None
+
+class Sanador(Detector):
+   
+    def __init__(self, matriz_adn):
+        super().__init__(matriz_adn)
+        
     
     def sanar_mutantes(self):
-        pass
+        lista_bases=['A','G','C','T']
+        
+        while self.detectar_mutantes()==True:
+            if not self.detectar_mutantes():
+                break
+            if self.detectar_horizontal():
+                fila_inicial,columna_inicial=self.posicion_mutante[0]
+
+                fila_mutada=list(self.matriz_adn[fila_inicial])
+                for i in range (columna_inicial,columna_inicial+4):
+                    fila_mutada[i]=random.choice(lista_bases)
+                self.matriz_adn[fila_inicial]=''.join(fila_mutada)
+                self.posicion_mutante.pop(0)
+                
+            elif self.detectar_vertical():
+                fila_inicial,columna_inicial=self.posicion_mutante[0]
+
+                for i in range(fila_inicial,fila_inicial+4):
+                    fila_mutada=list(self.matriz_adn[i])
+                    fila_mutada[columna_inicial]=random.choice(lista_bases)
+                    self.matriz_adn[i]=''.join(fila_mutada)
+                self.posicion_mutante.pop(0)
+                
+            elif self.detectar_diagonal():
+                fila_inicial,columna_inicial=self.posicion_mutante[0]
+
+                for i in range(4):
+                    fila=fila_inicial+i
+                    columna=columna_inicial+i
+                    fila_mutada=list(self.matriz_adn[fila])
+                    fila_mutada[columna]=random.choice(lista_bases)
+                    self.matriz_adn[fila]=''.join(fila_mutada)
+                self.posicion_mutante.pop(0)                
+            
+        return self.matriz_adn
